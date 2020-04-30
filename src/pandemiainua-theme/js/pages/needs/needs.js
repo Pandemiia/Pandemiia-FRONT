7ea62ac740217457d5b25a235fab7d8a4838c784
Lev <39336831+LevSaltsyn@users.noreply.github.com>
2020-04-30 19:37:29 +0300

    fix

--- src/pandemiainua-theme/js/pages/needs/needs.js
+++ src/pandemiainua-theme/js/pages/needs/needs.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Layout, TitlePage, ShowResult } from '@pinua/common/components';
-import Search from '@pinua/uikit/Search';
+import Search from '@pinua/uikit/search';
+import { Box } from '@pinua/uikit';
 import styles from './needs.scss';
 
 const Needs = () => {
   return (
     <Layout className={styles.page}>
-      <TitlePage title="Актуальні потреби" counter="1,087" />
-      <ShowResult total="366" />
-      <Search />
+      <Box direction="column">
+        <TitlePage title="Актуальні потреби" counter="1,087" />
+        <br />
+        <ShowResult total="366" />
+        <br />
+        <Search />
+      </Box>
     </Layout>
   );
 };
