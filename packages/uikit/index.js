3df54aa4368902049dbb1655313827732403f625
Obrevko Yurii <smile.oym@gmail.com>
2020-05-01 13:00:12 +0300

    drop down list

--- packages/uikit/index.js
+++ packages/uikit/index.js
@@ -2,3 +2,4 @@ export { default as Box } from './box';
 export { default as Text } from './text';
 export { default as Button } from './button';
 export { default as Search } from './Search';
+export { default as DropdownList } from './DropdownList';
--- src/pandemiainua-theme/js/pages/needs/needs.js
+++ src/pandemiainua-theme/js/pages/needs/needs.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Layout, TitlePage, ShowResult } from '@pinua/common/components';
-import Search from '@pinua/uikit/Search';
+// import Search from '@pinua/uikit/Search';
+import DropdownList from '@pinua/uikit/DropdownList';
+import { optionsList } from './constants';
 import styles from './needs.scss';
 
 const Needs = () => {
@@ -8,7 +10,9 @@ const Needs = () => {
     <Layout className={styles.page}>
       <TitlePage title="Актуальні потреби" counter="1,087" />
       <ShowResult total="366" />
-      <Search />
+
+      <DropdownList options={optionsList} />
+      {/* <Search /> */}
     </Layout>
   );
 };
