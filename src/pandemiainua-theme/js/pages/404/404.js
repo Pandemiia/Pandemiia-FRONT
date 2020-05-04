918a692502e7ba165fb8f6ed036b02a4f829914e
Artem Zhuravkov <navy2494@gmail.com>
2020-05-04 21:26:51 +0300

    404 page create

--- src/pandemiainua-theme/js/pages/404/404.js
+++ src/pandemiainua-theme/js/pages/404/404.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Box, Text, Button } from '@pinua/uikit';
+import styles from './404.scss';
 
 const NotFound = () => {
-  return <div>404 page not Found</div>;
+  return (
+    <Box className={styles.page} justify="center" align="center" direction="column">
+      <Text component="h1" size="xxl" className={styles.title}>
+        404
+      </Text>
+      <Text component="p" size="m" className={styles.subtext}>
+        Ooops... it seems you are lost
+      </Text>
+      <Button className={styles.button}>
+        <a href="/" className={styles.link}>
+          Go back home
+        </a>
+      </Button>
+    </Box>
+  );
 };
 
 export default NotFound;
