0b9019f536b23fde9ef204ade25503f6f98cf29e
Lev <39336831+LevSaltsyn@users.noreply.github.com>
2020-05-04 14:41:06 +0300

    fix dev, delete unused components

--- packages/common/components/index.js
+++ packages/common/components/index.js
@@ -3,7 +3,6 @@ export { default as Header } from './header';
 export { default as Footer } from './footer';
 export { default as SideBar } from './sidebar';
 export { default as Logo } from './logo';
-export { default as TitlePage } from './titlePage';
 export { default as ShowResult } from './showResult';
 export { default as Filter, ToggleScrollButtons, ToggleSelection } from './filter';
 export { DesktopMenu, MobileMenu } from './menu';
