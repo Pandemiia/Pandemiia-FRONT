4f88f0e3b16c5d4d08b630014ec65249887f6b2c
Lev <39336831+LevSaltsyn@users.noreply.github.com>
2020-05-06 01:12:26 +0300

    add 404 page, add warning massage in pages

--- src/pandemiainua-theme/js/pages/home/home.js
+++ src/pandemiainua-theme/js/pages/home/home.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Layout } from '@pinua/common/components';
+import { Text } from '@pinua/uikit';
 
 import styles from './home.scss';
 
 const Home = () => {
-  return <Layout className={styles.page}>Home</Layout>;
+  return (
+    <Layout className={styles.page}>
+      <Text>
+        Сторінка <b>Головна</b> знаходиться на стадіії розробки...
+      </Text>
+    </Layout>
+  );
 };
 
 export default Home;
